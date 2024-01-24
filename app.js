697a12f7cfe4cd6fcfe6df2c2f14058e643e75f0
Natalia Peripolli <natalia.peripolli@aerolineas.com.ar>
2024-01-24 12:43:03 -0300

    Se implementa handlebars

--- app.js
+++ app.js
@@ -5,16 +5,42 @@ const ProductManager = require("./classes/ProductManager");
 const CartManager = require("./classes/CartManager");
 const productManager = new ProductManager();
 const cartManager = new CartManager();
-import express from "express";
-import handlebars from "express-handlebars";
-import __dirname from "./utils";
+const exphbs = require('express-handlebars');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const productRouter = require("./routers/ProductRouter")(productManager);
+
+app.get('/', (req, res) => {
+    const products = productManager.getProducts();
+    res.render('home', { products });
+});
+
+app.get('/realtimeproducts', (req, res) => {
+    const products = productManager.getProducts();
+    res.render('realtimeProducts', { products });
+});
+
+io.on('connection', (socket) => {
+    console.log('Usuario conectado');
+
+    socket.emit('productsUpdated', productManager.getProducts());
+
+    socket.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    });
+});
+
 const cartRouter = require("./routers/CartRouter")(express, cartManager);
 
 app.use(express.json());
 
 const PORT = 8080;
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+app.use(express.static("public"));
+
 app.listen(PORT, () => {
     console.log(`Aplicación funcionando en el puerto ${PORT}`);
 });
